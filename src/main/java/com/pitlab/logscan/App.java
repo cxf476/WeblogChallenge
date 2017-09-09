d7dc471d9be3b54307683f2d0f3a747319ab26fd
xuefeng chang <cxf476@gmail.com>
2017-09-08 21:53:07 -0400

    create maven project

---
+++ src/main/java/com/pitlab/logscan/App.java
@@ -0,0 +1,13 @@
+package com.pitlab.logscan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
