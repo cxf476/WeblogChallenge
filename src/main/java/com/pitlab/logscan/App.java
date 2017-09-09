35dfe7e1a8f70fba6dcca230d658a412583df493
xuefeng chang <cxf476@gmail.com>
2017-09-08 22:42:53 -0400

    setup flink dependency

--- src/main/java/com/pitlab/logscan/App.java
+++ src/main/java/com/pitlab/logscan/App.java
@@ -1,13 +1,31 @@
 package com.pitlab.logscan;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+    	ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		List<String> list= new ArrayList<>();
+		for(int i =0; i<700000;++i) {
+			list.add("A");
+		}
+		DataSet<String> ds = env.fromCollection(list);
+		//List<DataSet<String>> dds = Lists.newArrayList();
+		for(int i=0;i<1000;++i) {
+			//dds.add(ds);
+			ds = ds.map(t->t);
+			ds.collect();
+			Thread.sleep(1000);
+		}
     }
 }
